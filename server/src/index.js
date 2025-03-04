2459d6e527e4653d72c7e2ad606d9e662180c0d4
shoc71 <supersonicwind6@gmail.com>
2025-03-04 05:52:25 -0500

    test push (again x14)

--- server/src/index.js
+++ server/src/index.js
@@ -23,11 +23,11 @@ app.use("/api/tasks", require("./routes/tasks")); // Example route for tasks
 
 // Serve React build in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
   // For all other routes, send back the React app
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
   });
 }
 
