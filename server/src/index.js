32dfef20c9fb359e3319be42747ce9089c0053b1
shoc71 <supersonicwind6@gmail.com>
2025-03-04 05:22:59 -0500

    test push (again x9)

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5001;
