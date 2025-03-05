81f14178aee2055286befaf33f812fa9927aaddd
shoc71 <supersonicwind6@gmail.com>
2025-03-05 18:26:38 -0500

    test push again x81

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = require("../config/database");
+const sequelize = require("../config/db");
 
 const Task = require("./tasks")(sequelize, Sequelize.DataTypes);
 
